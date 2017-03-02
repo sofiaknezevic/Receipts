3b5b28cff896e6f1f57fb00506ac451b139b6d8a
sofiaknezevic <sofia.knezevic@gmail.com>
2017-03-02 08:39:20 -0800

    Initial commit

---
+++ Receipts/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Receipts
+//
+//  Created by Sofia Knezevic on 2017-03-02.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts
+//
+//  Created by Sofia Knezevic on 2017-03-02.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
